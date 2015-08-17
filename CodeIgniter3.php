22a9e382709951c11811416055e6f10475fc1b2c
Wenpeng <imwwp@outlook.com>
2015-08-17 09:52:51 +0800

    Add Codeigniter3's helper file.

---
+++ CodeIgniter3.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @package	CodeIgniter
+ * @author	Wenpeng
+ * @link	http://pengblog.com
+ * @since	2015/4/16 9:19
+ * @version 1.0.0
+ */
+
+/**
+ * Add you custom models here
+ * ---------------------- Custome modules ----------------------
+ * You can add you custom libraries here! 
+ * Like:
+ * @property Test               		$test
+ *
+ * ---------------------- CodeIgniter modules ----------------------
+ * @property CI_Benchmark               $benchmark
+ * @property CI_Cache                   $cache
+ * @property CI_Calendar                $calendar
+ * @property CI_Cart                    $cart
+ * @property CI_Config                  $config
+ * @property CI_Email                   $email
+ * @property CI_Encrypt                 $encrypt
+ * @property CI_Exceptions              $exceptions
+ * @property CI_Upload                  $upload
+ * @property CI_Form_validation         $form_validation
+ * @property CI_Ftp                     $ftp
+ * @property CI_Image_lib               $image_lib
+ * @property CI_Input                   $input
+ * @property CI_Javascript              $javascript
+ * @property CI_Lang                    $lang
+ * @property CI_Loader                  $load
+ * @property CI_Migration               $migration
+ * @property CI_Output                  $output
+ * @property CI_Pagination              $pagination
+ * @property CI_Parser                  $parser
+ * @property CI_Security                $security
+ * @property CI_Session                 $session
+ * @property CI_Table                   $table
+ * @property CI_Trackback               $trackback
+ * @property CI_Typography              $typography
+ * @property CI_Unit_test               $unit_test
+ * @property CI_URI                     $uri
+ * @property CI_User_agent              $agent
+ * @property CI_Xmlrpc                  $xmlrpc
+ * @property CI_Xmlrpcs                 $xmlrpcs
+ * @property CI_Zip                     $zip
+ * @property CI_DB_query_builder        $db
+ */
+class PhpStorm_Helper 
+{
+
+}
+class CI_Controller extends PhpStorm_Helper 
+{
+	public function __construct(){}
+}
+class CI_Model extends PhpStorm_Helper 
+{
+	public function __construct(){}
+}
